ef7ba1c02f3a1e02d99854d10d2797c28af6e4d1
Javier Almeida <javier.a.almeida30@gmail.com>
2025-04-24 12:13:31 -0300

    add security groups for public and private instances, allow grafana port or public EC2 and PostgreSQL for private EC2

--- main.tf
+++ main.tf
@@ -9,3 +9,8 @@ module "vpc" {
   private_subnet_cidrs = var.private_subnet_cidrs
   azs                 = var.azs
 }
+
+module "security_groups" {
+  source = "./modules/security_groups"
+  vpc_id = module.vpc.vpc_id
+}
