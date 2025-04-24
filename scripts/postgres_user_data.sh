ac8832309dbf9e3b8b0550a639594e11dc2cc1a1
Javier Almeida <javier.a.almeida30@gmail.com>
2025-04-24 15:13:07 -0300

    Create bucket S3, fix user_data, add new script for daily logs

--- scripts/postgres_user_data.sh
+++ scripts/postgres_user_data.sh
@@ -4,9 +4,13 @@ sudo yum install -y docker
 sudo service docker start
 sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
+
+sudo docker volume create postgres-data
 sudo docker run -d \
   --name postgres \
+  --restart always \
   -e POSTGRES_USER=admin \
   -e POSTGRES_PASSWORD=adminpass \
   -p 5432:5432 \
+  -v postgres-data:/var/lib/postgresql/data \
   postgres
