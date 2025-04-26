f6b53d7f38193d38d29ab23a7a5421acbab6c0df
Javier Almeida <javier.a.almeida30@gmail.com>
2025-04-26 17:47:31 -0300

    rename script ec2_logs_to_s3.py

--- scripts/setup_scripts.sh
+++ scripts/setup_scripts.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# Variables
 BASTION_IP="Public_IP"
 KEY_PATH="~/.ssh/prueba-tecnica.pem"
 REMOTE_USER="ec2-user"
 
-# Subir los scripts
-scp -i $KEY_PATH ./scripts/postgresql_daily_backup.sh $REMOTE_USER@$BASTION_IP:/home/ec2-user/scripts/
-scp -i $KEY_PATH ./scripts/upload_logs.py $REMOTE_USER@$BASTION_IP:/home/ec2-user/scripts/
+scp -i "$KEY_PATH" ./scripts/postgresql_daily_backup.sh "$REMOTE_USER@$BASTION_IP:/home/ec2-user/scripts/"
+scp -i "$KEY_PATH" ./scripts/ec2_logs_to_s3.py "$REMOTE_USER@$BASTION_IP:/home/ec2-user/scripts/"
 
-# Dar permisos de ejecución
-ssh -i $KEY_PATH $REMOTE_USER@$BASTION_IP "chmod +x /home/ec2-user/scripts/*.sh"
+ssh -i "$KEY_PATH" "$REMOTE_USER@$BASTION_IP" "chmod +x /home/ec2-user/scripts/*.sh /home/ec2-user/scripts/*.py"
 
-# Configurar el crontab
-ssh -i $KEY_PATH $REMOTE_USER@$BASTION_IP << 'EOF'
-(crontab -l 2>/dev/null; echo "0 2 * * * /usr/bin/python3 /home/ec2-user/scripts/upload_logs.py >> /home/ec2-user/cron_logs.log 2>&1") | crontab -
+ssh -i "$KEY_PATH" "$REMOTE_USER@$BASTION_IP" << 'EOF'
+(crontab -l 2>/dev/null; echo "0 2 * * * /usr/bin/python3 /home/ec2-user/scripts/ec2_logs_to_s3.py >> /home/ec2-user/cron_logs.log 2>&1") | crontab -
 (crontab -l 2>/dev/null; echo "0 3 * * * /home/ec2-user/scripts/postgresql_daily_backup.sh >> /home/ec2-user/cron_pg_backup.log 2>&1") | crontab -
+EOF
+
+ssh -i "$KEY_PATH" "$REMOTE_USER@$BASTION_IP" << 'EOF'
+/usr/bin/python3 /home/ec2-user/scripts/ec2_logs_to_s3.py
+/home/ec2-user/scripts/postgresql_daily_backup.sh
 EOF
 
 echo "Scripts working on $BASTION_IP"
